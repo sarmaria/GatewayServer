69e8bad662f6df1abe78b73b70b95fcd6a01a833
charantechie <charantechie@gmail.com>
2026-03-30 15:39:57 +0200

    Adding response time to response headers

--- src/main/java/com/banking/gatewayserver/config/RouteConfig.java
+++ src/main/java/com/banking/gatewayserver/config/RouteConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Instant;
+
 @Configuration
 public class RouteConfig {
     private static final String ACCOUNTS_WITH_BAKNING_URI_PATH = "/banking/accounts/";
@@ -13,21 +15,25 @@ public class RouteConfig {
     private static final String ACCOUNTS_SERVICE_NAME = "ACCOUNTS";
     private static final String CARDS_SERVICE_NAME = "CARDS";
     private static final String LOANS_SERVICE_NAME = "LOANS";
+    public static final String RESPONSE_TIME_HEADER = "X-Response-Time";
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(route -> route
                         .path(ACCOUNTS_WITH_BAKNING_URI_PATH +"**")
-                        .filters(uri -> uri.rewritePath(ACCOUNTS_WITH_BAKNING_URI_PATH+"(?<segment>.*)", "/${segment}"))
+                        .filters(uri -> uri.rewritePath(ACCOUNTS_WITH_BAKNING_URI_PATH+"(?<segment>.*)", "/${segment}")
+                                .addResponseHeader(RESPONSE_TIME_HEADER, Instant.now().toString()))
                         .uri("lb://"+ACCOUNTS_SERVICE_NAME))
                 .route(route -> route
                         .path(CARDS_WITH_BAKNING_URI_PATH +"**")
-                        .filters(uri -> uri.rewritePath(CARDS_WITH_BAKNING_URI_PATH+"(?<segment>.*)", "/${segment}"))
+                        .filters(uri -> uri.rewritePath(CARDS_WITH_BAKNING_URI_PATH+"(?<segment>.*)", "/${segment}")
+                                .addResponseHeader(RESPONSE_TIME_HEADER, Instant.now().toString()))
                         .uri("lb://"+CARDS_SERVICE_NAME))
                 .route(route -> route
                         .path(LOANS_WITH_BAKNING_URI_PATH +"**")
-                        .filters(uri -> uri.rewritePath(LOANS_WITH_BAKNING_URI_PATH+"(?<segment>.*)", "/${segment}"))
+                        .filters(uri -> uri.rewritePath(LOANS_WITH_BAKNING_URI_PATH+"(?<segment>.*)", "/${segment}")
+                                .addResponseHeader(RESPONSE_TIME_HEADER, Instant.now().toString()))
                         .uri("lb://"+LOANS_SERVICE_NAME))
                 .build();
     }
